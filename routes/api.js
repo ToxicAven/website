6b16593f90501dc7c5b6c87467450009e76e6a3d
Jacob Herd <jacobherd229543@gmail.com>
2021-06-22 22:57:13 -0400

    Remove serve-favicon

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var app = express()
-
-app.use(require('serve-favicon')('./public/images/favicon.ico'));
 
 router.get('/', function(req, res) {
     res.send({ success: true, message: `AvenAPI`, endpoints: {uwu: `Makes a string super cute! :3`}, timestamp: Date.now()})
--- routes/yes.js
+++ routes/yes.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
-
-app.use(require('serve-favicon')('./public/images/favicon.ico'));
 
 router.get('/', function(req, res, next) {
   res.render('yes');
